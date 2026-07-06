45e28faf7f0795f5079165f3cbad073b45826126
nbnjdhbq2309-beep <nbnjd.hbq2309@gmail.com>
2026-07-06 16:17:17 +0300

    Update MainActivity.kt

--- package-android-src/MainActivity.kt
+++ package-android-src/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.uniqdev.meetingrecorder
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         registerPlugin(MeetingRecorderPlugin::class.java)
         super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val found = try {
+                bridge?.getPlugin("MeetingRecorder") != null
+            } catch (e: Exception) {
+                false
+            }
+            Toast.makeText(
+                this,
+                "[Native diag] MainActivity.onCreate выполнен. MeetingRecorder в Bridge: ${if (found) "ЕСТЬ" else "НЕТ"}",
+                Toast.LENGTH_LONG
+            ).show()
+        }, 1500)
     }
 }
